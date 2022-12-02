e24acae592cc0e1c662a36afe99108e05e02e176
Matthew Puchyr <matt.puchyr@gmail.com>
2022-12-02 12:05:40 -0600

    Create model for occupation

--- app/models/character.rb
+++ app/models/character.rb
@@ -1,5 +1,6 @@
 class Character < ApplicationRecord
   belongs_to :owner, class_name: "User"
+  belongs_to :occupation
 
   enum pronoun: { he: "he/him", she: "she/her", they: "they/them"}
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_12_02_173937) do
+ActiveRecord::Schema[7.0].define(version: 2022_12_02_180354) do
   create_table "characters", force: :cascade do |t|
     t.integer "owner_id", null: false
     t.string "first_name"
@@ -34,6 +34,12 @@ ActiveRecord::Schema[7.0].define(version: 2022_12_02_173937) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["owner_id"], name: "index_characters_on_owner_id"
+  end
+
+  create_table "occupations", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
