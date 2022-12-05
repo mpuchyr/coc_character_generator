76adb79ec3360ed04d68d88b8f8663ba2ebfd6aa
Matthew Puchyr <matt.puchyr@gmail.com>
2022-12-05 09:51:02 -0600

    Add a root route in routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ Rails.application.routes.draw do
   devise_scope :user do
     get '/users/sign_out' => 'devise/sessions#destroy'
   end
+
+  root "characters#index"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
