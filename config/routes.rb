63f1d15094b2bf7d80fdb3d7c9095836f4b45b62
Matthew Puchyr <matt.puchyr@gmail.com>
2022-12-05 09:44:26 -0600

    Add rudimentary navbar and add fix in routes to allow user sign out

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   resources :characters
   devise_for :users
+
+  devise_scope :user do
+    get '/users/sign_out' => 'devise/sessions#destroy'
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
