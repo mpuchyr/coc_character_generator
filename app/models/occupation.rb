c5c42caa0fd3c054c7c75667d2bbdd753173f01e
mpuchyr <matt.puchyr@gmail.com>
2022-12-06 12:09:52 -0600

    Add skill values for Antiquarian in occupation.rb

--- app/models/occupation.rb
+++ app/models/occupation.rb
@@ -6,15 +6,27 @@ class Occupation < ApplicationRecord
         social_skills_choice_1 = all_social_skills.pop
         social_skills_choice_2 = all_social_skills.pop
 
+        skill_options = [70, 60, 60, 50, 50, 50, 40, 40, 40].shuffle
+
         case self.name
         when "Antiquarian"
-            return ["appraise", "art/craft", "history", "library_use", "language_other", "any", social_skills_choice_1]
+            return {
+                "appraise" => skill_options.pop,
+                "art_craft" => skill_options.pop,
+                # "art_craft_specialization_id" => ArtSpecialization.all.sample.id,
+                "history" => skill_options.pop,
+                "library_use" => skill_options.pop,
+                "language_other" => skill_options.pop,
+                social_skills_choice_1 => skill_options.pop,
+                "any" => skill_options.pop,
+                "credit_rating" => skill_options.pop
+            }  
         when "Artist"
-            return ["art/craft", ["history", "natural_world"].sample, social_skills_choice_1, "language_other", "psychology", "spot_hidden", "any", "any"]
+            return ["art_craft", ["history", "natural_world"].sample, social_skills_choice_1, "language_other", "psychology", "spot_hidden", "any", "any"]
         when "Athlete"
             return [["climb", "jump", "fighting_brawl"].sample, "ride", social_skills_choice_1, "swim", "throw", "any"]
         when "Author"
-            return ["art/craft", "history", "library_use", ["natural_world", "occult"].sample, "language_other", "language_own", "psychology", "any"]
+            return ["art_craft", "history", "library_use", ["natural_world", "occult"].sample, "language_other", "language_own", "psychology", "any"]
         end
     end
 end
